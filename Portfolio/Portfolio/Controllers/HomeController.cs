b383359b6bce70c5b8b9b7124aaadb6fbe229e64
Miguel <miguellofredo@gmail.com>
2022-12-29 20:40:19 -0300

    Update README.md

---
+++ Portfolio/Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using Portfolio.Services;
+using System.Diagnostics;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly RepoProjetos repoProjetos;
+
+        public HomeController(ILogger<HomeController> logger, RepoProjetos repoProjetos)
+        {
+            _logger = logger;
+            this.repoProjetos = repoProjetos;
+        }
+
+        
+
+        public IActionResult Index()
+        {
+            var projects = repoProjetos.GetProjects().Take(3).ToList();
+            var model  = new HomeIndexViewModel() { Projects = projects };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portfolio/Portfolio/Services/RepoProjetos.cs
@@ -0,0 +1,49 @@
+﻿using Portfolio.Models;
+
+namespace Portfolio.Services
+{
+    public class RepoProjetos
+    {
+        public List<Project> GetProjects()
+        {
+            return new List<Project>()
+            {
+                new Project
+                {
+                    Title="Amazon",
+                    Description="Buy anything",
+                    ImageURL= "/images/NicePng_messi-png_273780.png",
+                    Link= "https://www.amazon.com"
+                },
+                new Project
+                {
+                     Title="Mercadolivre",
+                    Description="Buy a lot of diferents stuffs",
+                    ImageURL= "/images/NicePng_messi-png_273780.png",
+                    Link= "https://www.mercadolivre.com.br"
+                },
+                new Project
+                {
+                     Title="Google",
+                    Description="Internet Seach Engine",
+                    ImageURL= "/images/NicePng_messi-png_273780.png",
+                    Link= "https://www.google.com"
+                },
+                new Project
+                {
+                     Title="PAgina12",
+                    Description="Argentine Newspaper",
+                    ImageURL= "/images/NicePng_messi-png_273780.png",
+                    Link= "https://www.pagina12.com"
+                },
+                new Project
+                {
+                     Title="Steam",
+                    Description="Videogames Store",
+                    ImageURL= "/images/NicePng_messi-png_273780.png",
+                    Link= "https://www.steam.com"
+                }
+            };
+        }
+    }
+}
