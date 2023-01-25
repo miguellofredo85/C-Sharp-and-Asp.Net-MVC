85a07d9e04d8dd8651a05dc0b7d6b23026afda66
Miguel <miguellofredo@gmail.com>
2023-01-25 16:30:24 -0300

    Moving files and cleaning

--- ManejoPresupuesto/ManejoPresupuesto/Models/IndiceCuentasViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ManejoPresupuesto.Models
-{
-    public class IndiceCuentasViewModel
-    {
-        public string TipoCuenta { get; set; }
-        public IEnumerable<Cuenta> Cuentas { get; set; }
-        public decimal Balance => Cuentas.Sum(x => x.Balance);
-    }
-}
